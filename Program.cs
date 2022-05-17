0a5a302a65481cb40237e8a31d84971685a65613
ShawnDKing <shawnking38061074@gmail.com>
2022-05-17 09:07:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KingS_Dialer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
